ee44a16bb2809227029d057d9d39d6c7a6777c8e
Divyansh Kushwaha <jatinkushwaha309@gmail.com>
2024-12-19 13:56:58 +0530

    pipeline configured without params

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -88,8 +88,8 @@ def main():
     try:
         # params = load_params(params_path='params.yaml')
         # test_size = params['data_ingestion']['test_size']
-        test_size = 0.2
-        data_path = 'https://raw.githubusercontent.com/DivyanshKushwaha/Spam-Detection-using-ML-Pipeline/refs/heads/main/experiments/spam.csv'
+        test_size = 0.21
+        data_path = 'https://raw.githubusercontent.com/DivyanshKushwaha/Spam-Detection-using-ML-Pipeline/refs/heads/master/experiments/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=2)
